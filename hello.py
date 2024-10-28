970aca3825bc809c494a87a6c3a05521accf6ed3
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-10-29 01:12:33 +0200

    add some comments

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
 # This program says hello and asks for my name
-
+# The print() function displays the string value inside its parentheses on the screen.
 print('Hello, world!');
 print('What is your name?'); # ask for their name
 
-myName = input();
+myName = input(); # The input() function waits for the user to type some text on the keyboard and press enter
 
 print('It is good to meet you, ' + myName);
 print('The length of your name is:')
