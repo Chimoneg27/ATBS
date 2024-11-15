f5664b723914fd73fa7c8be5efd856d1c7015c06
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-11-15 20:13:57 +0200

    practice loops for python

---
+++ fiveTimes.py
@@ -0,0 +1,8 @@
+# print("My name is")
+# for i in range(5):
+#     print('Jimmy Five Times (' + str(i) +')')
+g = 0
+print('My name is')
+while g < 5:
+    print('Jimmy Five Time (' + str(g) +')')
+    g = g + 1
---
+++ gauss.py
@@ -0,0 +1,4 @@
+total = 0
+for g in range(101):
+    total = total + g
+print(total)
