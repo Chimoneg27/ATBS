5a33b6c0b5173493b4fb3ea3014e175689ab918e
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-11-16 22:33:36 +0200

    add comments

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -6,8 +6,10 @@ print('I am thinking of a number between 1 and 20.')
 
 # Ask the player to guess 6 times.
 for guessTaken in range(1, 7):
+# the loop runs seven times
     print('Take a guess')
     guess = int(input())
+    # turns the input into an integer
 
     if guess < secretNumber:
         print('Your guess is too low.')
