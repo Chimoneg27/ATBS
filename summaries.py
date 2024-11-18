eb0588128835c90a51fbc3c33d5d58ad49a2af8d
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-11-18 18:48:28 +0200

    summary questions

---
+++ summaries.py
@@ -0,0 +1,12 @@
+# 2e Chapter2 Summary questions
+
+# 8 - 9
+spam = 3
+if spam == 10:
+    print('eggs')
+    if spam > 5:
+        print('bacon')
+    else:
+        print('ham')
+    print('spam')
+print('spam')
